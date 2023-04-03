ab7e27a61aff175e887717d24dedab9e9d79ddd5
Hurstonthan <than0@purdue.edu>
2023-04-03 00:56:58 -0400

    Distance Estimation changed

--- BackEnd Files/sketch.js
+++ BackEnd Files/sketch.js
@@ -54,6 +54,26 @@ function draw() {
         fill(nose_r,nose_g,nose_b);
         ellipse(nose.x, nose.y, 64);
 
+        let focal_length = 3.6 * (10 ** -3);
+        let the_height = 0.0385;
+        let right_ankle = pose.rightAnkle;
+        let left_ankle = pose.leftAnkle;
+        let right_Shoulder = pose.rightShoulder;
+        let left_Shoulder = pose.leftShoulder;
+        let right_Hip = pose.rightHip;
+        let left_Hip = pose.leftHip;
+        let nose_0 = pose.nose;
+        let bttm_average_x = (right_Shoulder.x + left_Shoulder.x) / 2;
+        let bttm_average_y = (right_Shoulder.y + left_Shoulder.y) / 2;
+        let image_height_1 = (dist (right_Shoulder.x, right_Shoulder.y,left_Shoulder.x, left_Shoulder.y)) * (0.0058 * 0.0001);
+        let image_height_2 = (dist (left_Shoulder.x, left_Shoulder.y,left_Hip.x, left_Hip.y)) * (0.0040 * 0.0001);
+        let actual_distance_1 = (the_height * focal_length) / image_height_1;        
+        let actual_distance_2 = (the_height * focal_length) / image_height_2;        
+        let leftShoulderScore = pose.keypoints.find(kp => kp.part === 'leftShoulder').score;
+        let marking = detections[0];
+
+        
+
 
         for(let i = 3; i < pose.keypoints.length; i++) {
             let x = pose.keypoints[i].position.x;
@@ -76,6 +96,25 @@ function draw() {
                     text(object.label + i, object.x + 10, object.y + 24);
                 }
 
+        if (leftShoulderScore >= 0.8)
+        {
+            noStroke();
+            fill (255,0,255);
+            textSize (26);
+            text("The estimated distance (m): ",marking.x + 10,470)
+            text(round(actual_distance_1,1),marking.x + 340,470);
+        }
+
+        else
+        {
+            noStroke();
+            fill (0,255,0);
+            textSize (26);
+            text("The estimated distance (m): ",marking.x + 10,470)
+            text(round(actual_distance_2,1),marking.x + 340,470);
+
+            
+        }
         //Testing
         }
     }
